97e09dc6d13b1094d40c37c59fca7ad7d72b36b2
StevenJCorreia <stevenjosephcorreia@gmail.com>
2019-07-13 22:30:38 -0400

    Added menubar actions and respective functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,96 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->note->copy();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->note->cut();
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->note->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->note->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->note->paste();
+}
+
+void MainWindow::on_actionPrint_triggered()
+{
+    QPrinter printer;
+    printer.setPrinterName("Printer Name");
+    QPrintDialog pDialog(&printer, this);
+    if(pDialog.exec() == QDialog::Rejected) {
+        QMessageBox::warning(this, "warning", "Cannot access printer");
+        return;
+    }
+    ui->note->print(&printer);
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->note->redo();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QFile file (currentFile);
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save current file");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->note->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as...");
+    QFile file (fileName);
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->note->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->note->undo();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QFile>
+#include <QFileDialog>
 #include <QDialog>
 #include <QTextStream>
 #include <QMessageBox>
@@ -20,6 +20,29 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_As_triggered();
+
+    void on_actionPrint_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionSave_triggered();
 
 private:
     Ui::MainWindow *ui;
